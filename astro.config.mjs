a11b140663cf5958dc91a075c7d785a5cb86030f
EBregains <ebregains@gmail.com>
2024-05-23 15:15:14 -0300

    Added "no" and "de" Locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'es',
-    locales: ['es', 'en', 'ca'],
+    locales: ['es', 'en', 'no', 'de'],
     routing: {
       prefixDefaultLocale: false
     }
